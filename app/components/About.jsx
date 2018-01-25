6381ff8cfd5e18f7258208cd5e9644953ad0e0b1
Kamalakannan Krishnaraj <35299129+KamalakannanKrishnaraj15@users.noreply.github.com>
2018-01-25 22:22:13 +0530

    Added foundation and CSS styles to the existing pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is an simple weather application build on React framework.</p>
+      <p> Here are some of the tools used: </p>
+      <ul>
+          <li>
+              <a href="https://facebook.github.io/react">React</a> - This is the JavaScript framework used.
+          </li>
+          <li>
+              <a href="http://openweathermap.org">Open Weather Map</a> - I used open weather map to search for weather data by city name.
+          </li>
+      </ul>
     </div>
   )
 };
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are the few examples try out:</p>
+      <ol>
+          <li>
+            <Link to="/?Location=Bengaluru"> Bengaluru IN</Link>
+          </li>
+          <li>
+          <Link to="/?Location=Chennai"> Chennai IN </Link>
+          </li>
+      </ol>
     </div>
   )
 };
